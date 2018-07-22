afb4c34fcfeffe2387b78de1522cf27a509a6275
IreriVikki <wambsviki@gmail.com>
2018-07-22 14:07:26 +0300

    initial commit of cha muhimu project

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var words = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quo eaque est itaque consequatur labore unde blanditiis porro ex provident expedita rerum, temporibus aliquid? Omnis porro ab sint nemo'
+
+var sysText = $('#sysText').text();
+
+console.log(sysText)
+
+var  userText = $('userText').val();
+
+
+
+for (;;){
+    
+}
