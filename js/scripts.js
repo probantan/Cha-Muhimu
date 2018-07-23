58fce70c13d31be377e3489f09806edc5e336148
IreriVikki <wambsviki@gmail.com>
2018-07-23 10:34:19 +0300

    project begining

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 var words = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quo eaque est itaque consequatur labore unde blanditiis porro ex provident expedita rerum, temporibus aliquid? Omnis porro ab sint nemo'
 
 var sysText = $('#sysText').text();
+$('#userText').text('');
+console.log(($('#userText').val().length))
 
-console.log(sysText)
+$('#userText').keyup(function (e) {
+    let userText = $('#userText').val();
+    console.log(userText)
+    console.log('sys text' + sysText.slice(0, userText.length))
+    if(userText === sysText.slice(0, userText.length)){
+        $('#userText').css('color', 'blue');
+    }else{
+        $('#userText').css('color', 'red');
+    }
+});
 
-var  userText = $('userText').val();
-
-
-
-for (;;){
-    
-}
+$('#words').keyup(function (e) { 
+    let userSentence = $('#words').text();
+    var userWord = userSentence.split(' ');
+    console.log(userWord)
+    userWord.forEach(function(word) {
+        console.log(word)
+        // let ids = 'word' + userWord.length - 1
+        // $('#words').before("<span>"+word+"</span>");
+    });
+    var currentId = "word" + userWord.length - 1
+    if (userText === sysText.slice(0, userText.length)) {
+        $(currentId).css('color', 'blue');
+    } else {
+        $(currentId).css('color', 'red');
+    }
+});
